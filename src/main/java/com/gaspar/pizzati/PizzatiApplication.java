1f262525f6f90b87684faffc3bc9a61acd9631dc
Gaspar <e972136@gmail.com>
2023-08-16 18:17:00 -0600

    Inicial

---
+++ src/main/java/com/gaspar/pizzati/PizzatiApplication.java
@@ -0,0 +1,13 @@
+package com.gaspar.pizzati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzatiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaspar/pizzati/PizzatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaspar.pizzati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
